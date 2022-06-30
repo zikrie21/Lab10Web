49d3bd47a8981ad31b5b796651b6556aba1a99f7
zikri21 <marifatululfah99@gmail.com>
2022-06-30 16:12:51 +0700

    lab10web

---
+++ form.php
@@ -0,0 +1,55 @@
+<?php
+/**
+* Nama Class: Form
+* Deskripsi: CLass untuk membuat form inputan text sederhana
+**/
+class Form
+{
+    private $fields = array();
+    private $action;
+    private $submit = "Submit Form";
+    private $jumField = 0;
+
+    public function __construct($action, $submit)
+    {
+        $this->action = $action;
+        $this->submit = $submit;
+    }
+
+    public function displayForm()
+    {
+        echo "<form action='".$this->action."' method='POST'>";
+        echo '<table width="100%" border="0">';
+        for ($j=0; $j<count($this->fields); $j++) {
+            echo "<tr><td
+            align='right'>".$this->fields[$j]['label']."</td>";
+            echo "<td><input type='text'
+            name='".$this->fields[$j]['name']."'></td></tr>";
+        }
+        echo "<tr><td colspan='2'>";
+        echo "<input type='submit' value='".$this->submit."'></td></tr>";
+        echo "</table>";
+    }
+
+    public function addField($name, $label)
+    {
+        $this->fields [$this->jumField]['name'] = $name;
+        $this->fields [$this->jumField]['label'] = $label;
+        $this->jumField ++;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab10Web</title>
+</head>
+<body>
+    
+</body>
+<footer>312010069 ||  ZIKRI HADIANSYAH</footer>
+</html>
